21471d7db8c56d2a8439b374759a691134c4a1f2
scarlett <youjin9200@naver.com>
2018-08-05 12:02:35 +0900

    Create 2309.java

---
+++ 2309.java
@@ -0,0 +1,31 @@
+/*
+2309 - 일곱 난쟁이
+9C7 == 9C2
+*/
+package algorithm_main;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+        	arr[i] = sc.nextInt();
+        	sum += arr[i];
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i <= 8; i++) {
+        	for (int j = i+1; j <= 8; j++) {
+        		if ((sum - (arr[i]+arr[j])) == 100) {
+        			for (int k = 0; k < 9; k++) {
+        				if (k == i || k == j)
+        					continue;
+        	        	System.out.println(arr[k]);
+        	        }
+        			return;
+        		}
+        	}
+        }
+    }
+}
